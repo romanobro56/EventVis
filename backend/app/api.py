31b07ae35a97a4b58308ac1f0c0b387d2590d22f
alan-cot0 <160337699+alan-cot0@users.noreply.github.com>
2025-11-16 22:39:09 -0500

    Almost fixed getting the Clerk API Key.
    The problem is that line 6, `from unique_keys import CLERK_SECRET_KEY` fails to execute because the venv doesn't think that the file `unique_keys.py` is not a module. (The file exists in the right place on my machine, and it's git ignored.)

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from clerk_backend_api import Clerk, Session
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from unique_keys import CLERK_SECRET_KEY
 
 app = FastAPI()
 
@@ -15,8 +16,8 @@ origins = [
 
 load_dotenv()
 
-# I don't know how to get a Clerk API Key yet.
-clerk = Clerk(api_key=os.getenv("CLERK_API_KEY"))
+# I don't know if I'm getting a Clerk API Key correctly.
+clerk = Clerk(bearer_auth=os.getenv(CLERK_SECRET_KEY))
 
 app.add_middleware(
     CORSMiddleware,
