d74f3ba833ee3a84d995caed71a1e38e6f0dda40
Linnea Berryman <linnea.berryman@gmail.com>
2025-11-24 22:17:28 -0500

    Adding build.md file, adding __init__files where appropriate, base test connection for mongodb

--- db/database_client.py
+++ db/database_client.py
@@ -1,23 +1,78 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import ConnectionFailure
 
 import os
 
+#set your MongoDB username and password to these environment variables
 username = os.environ.get('USRNM')
 password = os.environ.get('PASS')
 
-print(username)
-print(password)
 
+
+#generate the connection string
 uri = f"mongodb+srv://{username}:{password}@eventviscluster.dpmc4qx.mongodb.net/?appName=EventVisCluster"
 
+def connect_cluster():
+    # Create a new client and connect to the server
+    try:
+        client = MongoClient(uri, server_api=ServerApi('1'))
+    except ConnectionFailure as e:
+        return e
+    return client
+    
+def close_connection(client):
+    
+    return
+
+def create_user(user: str):
+    #TODO
+    return
+
+def update_events_campus_pulse():
+    return
+
+def update_events_calendar():
+    return
+
+def create_new_event(event: str):
+    #TODO
+    return
+   
+
+def delete_event(event: str):
+    #TODO
+    return
+  
+
+def get_events(query: str):
+    #TODO
+    return
+   
+
+def delete_user(user: str):
+    #TODO
+    return
+
+
+# Send a ping to confirm a successful connection
+
+
+#for testing
+print(username)
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    database = client.get_database("sample_mflix")
+    movies = database.get_collection("movies")
+    # Query for a movie that has the title 'Back to the Future'
+    query = { "title": "Back to the Future" }
+    movie = movies.find_one(query)
+    print(movie)
+    client.close()
 except Exception as e:
     print(e)
+
+        
