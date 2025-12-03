58be51552ebfd2ca2505033c1efd2d964619b5a1
alan-cot0 <160337699+alan-cot0@users.noreply.github.com>
2025-12-03 01:48:27 -0500

    Added the very beginning of a testing suite.

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# The files might not be in their final directories.
+import unittest
+
+from backend.app import User
---
+++ tests/login_tests.py
@@ -0,0 +1,18 @@
+from __init__ import *
+
+# Sample user data to test
+sampleUser1 = {
+    "name": "Noelle",
+    "email": "noelle123@example.com",
+    "password": "holidays"
+}
+# I don't think we've decided on user ID assignment yet.
+
+class LoginTestSuite(unittest.TestCase):
+    user = None
+
+    def test_duplicate_emails(self):
+        pass # TODO
+
+    def test_login_success_after_account_creation(self):
+        pass
